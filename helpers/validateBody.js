760be6be8716d9fbf3e431273a65059054a62a10
InnaKolesnykova <149619711+InnaKolesnykova@users.noreply.github.com>
2025-09-11 11:20:51 +0300

    Update validateBody.js

--- helpers/validateBody.js
+++ helpers/validateBody.js
@@ -2,12 +2,13 @@ import HttpError from "./HttpError.js";
 
 const validateBody = (schema) => {
   const func = (req, _, next) => {
-    const { error } = schema.validate(req.body);
-    if (error) {
-      next(HttpError(400, error.message));
-    }
-    next();
-  };
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return next(HttpError(400, error.message));
+  }
+  next();
+};
+
 
   return func;
 };
