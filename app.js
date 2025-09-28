59791112685774e2fcee8875256ba4f0217092dd
InnaKolesnykova <149619711+InnaKolesnykova@users.noreply.github.com>
2025-09-28 12:19:30 +0300

    h
    h

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
-
-import contactsRouter from "./routes/contactsRouter.js";
+import express from 'express';
+import authRouter from './routes/auth.js';
+import contactsRouter from './routes/contactsRouter.js';
+import connectDB from './db/db.js';
 
 const app = express();
 
-app.use(morgan("tiny"));
-app.use(cors());
+connectDB();
+
 app.use(express.json());
 
-app.use("/api/contacts", contactsRouter);
+app.use('/users', authRouter);
+app.use('/api/contacts', contactsRouter);
 
-app.use((_, res) => {
-  res.status(404).json({ message: "Route not found" });
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'Route not found' });
 });
 
 app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
-  res.status(status).json({ message });
+  console.error(err.stack);
+  res.status(500).json({ message: 'Internal Server Error' });
 });
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
