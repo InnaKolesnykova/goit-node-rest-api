e9ceff9c3451c8fca52ce620c7c4cedfbb745f8b
InnaKolesnykova <149619711+InnaKolesnykova@users.noreply.github.com>
2025-10-19 15:01:20 +0300

    т
    т

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { register, loginUser, logoutUser, getCurrentUser, updateAvatar } from '../controllers/userControllers.js';
 import authMiddleware from '../middleware/authMiddleware.js';
+import upload from '../middleware/upload.js';
 
 const authRouter = express.Router();
 
@@ -8,6 +9,10 @@ authRouter.post('/register', register);
 authRouter.post('/login', loginUser);
 authRouter.post('/logout', authMiddleware, logoutUser);
 authRouter.get('/current', authMiddleware, getCurrentUser);
-authRouter.patch('/avatar', authMiddleware, updateAvatar)
+authRouter.patch('/avatar', authMiddleware, upload.single('avatar'), updateAvatar);
+
+authRouter.get('/avatars', (req, res) => {
+  res.json({ message: 'Avatars route works!' });
+});
 
 export default authRouter;
