eb357d93d0c517c2c635aa001cb67c6eedc56cc7
InnaKolesnykova <149619711+InnaKolesnykova@users.noreply.github.com>
2025-10-20 06:55:27 +0300

    и
    и

--- app.js
+++ app.js
@@ -18,9 +18,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/users/avatar', upload.single('avatar'));
-
 app.use('/users', authRouter);
 app.use('/api/contacts', contactsRouter);
 
